8926c4b268be8ec9cd04bab80a61bf342d74d496
Rafael Augusto <rafaelaugusto.developer@gmail.com>
2019-03-17 23:59:37 -0400

    refactor: Refactor the file 'App' for add Provider Store redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Root } from 'native-base'
-
+import { Provider } from 'react-redux'
 import Routes from './routes'
+import store from './store'
 
 const App = () => {
     return(
-        <Root>
-            <Routes />
-        </Root>
+        <Provider store={store}>
+            <Root>
+                <Routes />
+            </Root>
+        </Provider>
     )
 }
 
